27720b68abeb86edad110943ea2fac8eaf7fecd6
ascension <jerroddb@gmail.com>
2023-04-11 17:11:57 -0600

    fix(hardhat): build error

--- apps/hardhat/docker/entrypoint.sh
+++ apps/hardhat/docker/entrypoint.sh
@@ -4,7 +4,7 @@
 npx pm2 start 'yarn start:local'
 
 # Wait for hardhat node to initialize and then deploy contracts
-npx wait-on http://127.0.0.1:8545 && yarn deploy:local;
+# npx wait-on http://127.0.0.1:8545 && yarn deploy:local;
 
 # The hardhat node process never completes
 # Waiting prevents the container from pausing
