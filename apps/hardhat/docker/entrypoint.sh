ee59c7e08850f7825640ec2fffe17d1bf54edba1
ascension <jerroddb@gmail.com>
2023-04-11 15:54:48 -0600

    fix(hardhat): entrypoint via pm2

--- apps/hardhat/docker/entrypoint.sh
+++ apps/hardhat/docker/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Start hardhat node as a background process
-yarn start:local &
+npx pm2 start 'yarn start:local'
 
 # Wait for hardhat node to initialize and then deploy contracts
 npx wait-on http://127.0.0.1:8545 && yarn deploy:local;
 
 # The hardhat node process never completes
 # Waiting prevents the container from pausing
-wait $!
+# wait $!
