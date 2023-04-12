467f4866dfbcf592ab9e7404dd825885b71abfae
ascension <jerroddb@gmail.com>
2023-04-11 19:06:35 -0600

    fix(hardhat): entrypoint v70

--- apps/hardhat/docker/entrypoint.sh
+++ apps/hardhat/docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Start hardhat node as a background process
-nohup yarn start:local
+yarn start:local
 # Wait for hardhat node to initialize and then deploy contracts
 # npx wait-on http://127.0.0.1:8545 && yarn deploy:local;
 
