f77cf76fb6456615f3d56151c9e1df8c7fd15dca
ascension <jerroddb@gmail.com>
2023-04-17 12:21:34 -0600

    fix(hardhat): deploy issue

--- apps/hardhat/docker/entrypoint.sh
+++ apps/hardhat/docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Start hardhat node as a background process
-yarn start:local
+yarn start:prod
 # Wait for hardhat node to initialize and then deploy contracts
 # npx wait-on http://127.0.0.1:8545 && yarn deploy:local;
 
